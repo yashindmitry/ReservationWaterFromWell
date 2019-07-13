a6eeab3a0d15ca85352d1d0d4bf0dcd59c8e0765
yashindmitry <to.yashin@gmail.com>
2019-07-13 17:23:44 +0400

    Ушел от delay внутри loop

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,6 +1,6 @@
 
-int RelayWell = 4; // Реле управление соленоидным клапаном на колодце
-int RelayBarrel = 6; // Реле управление соленоидным клапаном на бочке
+const int RelayWell = 4; // Реле управление соленоидным клапаном на колодце
+const int RelayBarrel = 6; // Реле управление соленоидным клапаном на бочке
 
 
 void setup() {
@@ -10,24 +10,36 @@ void setup() {
   pinMode(RelayBarrel, OUTPUT);
 }
 
+long lastCheckMillis = 0; // Время последней проверки уровня воды в колодце
+long checkInterval = 1000; // Через сколько осуществить следующую проверку
+
 void loop() {
-  int IsHighWaterLevel = digitalRead(A0); // Читаем данные с датчика уровня воды
-  Serial.println(IsHighWaterLevel); // Печатаем в консоль
-  digitalWrite(LED_BUILTIN, IsHighWaterLevel); // Выставляем диод
-  if (IsHighWaterLevel == HIGH) {
-    // Открываем кран колодца
-    digitalWrite(RelayWell, LOW);
-    // Закрываем кран из бочки
-    digitalWrite(RelayBarrel, LOW);
-    // Следующую проверку уровня воды в колодце сделаем через 1 минуту
-    delay(60000);
-  } else {
-    // Открываем кран из бочки
-    digitalWrite(RelayBarrel, HIGH);
-    // Закрываем кран колодца
-    digitalWrite(RelayWell, HIGH);
-    // Следующую проверку уровня воды в колодце сделаем через 10 минут
-    delay(600000);
+
+  unsigned long currentMillis = millis();
+
+  // Проверим не пришло ли время проверять уровень воды в колодце
+  if(currentMillis - lastCheckMillis > checkInterval) {
+    Serial.print("Проверяю уровень воды в колодце: ");
+    lastCheckMillis = currentMillis;
+    int IsHighWaterLevel = digitalRead(A0); // Читаем данные с датчика уровня воды
+    digitalWrite(LED_BUILTIN, IsHighWaterLevel); // Выставляем диод
+    if (IsHighWaterLevel == HIGH) {
+      Serial.println("высокий");
+      // Открываем кран колодца
+      digitalWrite(RelayWell, LOW);
+      // Закрываем кран из бочки
+      digitalWrite(RelayBarrel, LOW);
+      Serial.println("Следующую проверку уровня воды в колодце сделаем через 1 минуту");
+      checkInterval = 60000;
+    } else {
+      Serial.println("низкий");
+      // Открываем кран из бочки
+      digitalWrite(RelayBarrel, HIGH);
+      // Закрываем кран колодца
+      digitalWrite(RelayWell, HIGH);
+      Serial.println("Следующую проверку уровня воды в колодце сделаем через 10 минут");
+      checkInterval = 600000;
+    }
   }
-  delay(1000);
+
 }
